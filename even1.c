a44d27102aca0b1699392b888fe5f6af725e8d09
balakumardevil <42169755+balakumardevil@users.noreply.github.com>
2018-08-22 07:33:52 +0530

    Create even1.c

---
+++ even1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr;
+printf("enter the element");
+scanf("%d",a)
+if(a>0)
+{
+printf("positive");
+}
+else if(a<o)
+{
+printf("negative");
+}
+else
+{
+printf("zero");
+}
+}

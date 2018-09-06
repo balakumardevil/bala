2a92938f0c40f3531ad231f282a3ce178da9153d
balakumardevil <42169755+balakumardevil@users.noreply.github.com>
2018-09-06 11:57:29 +0530

    Create alphabets.java

---
+++ alphabets.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class even
+{
+public static void main(String[] arg)
+{
+char ch;
+Scanner s=new Scanner(System.in);
+System.out.println("enter the element");
+ch=s.next(".").charAt(0);
+if(ch>='a'&&ch<='z')
+{
+System.out.println("alphabets");
+}
+else
+{
+System.out.println("numbers");
+}
+}
+}

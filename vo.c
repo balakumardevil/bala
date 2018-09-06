456d9e86b9442aa5f62d5eb82a93bbb9dd4d02bc
balakumardevil <42169755+balakumardevil@users.noreply.github.com>
2018-09-06 11:09:47 +0530

    Create vo.c

---
+++ vo.c
@@ -0,0 +1,15 @@
+include<stdio.h>
+include<conio.h>
+void main()
+{
+int n;
+printf("enter the alphaphet");
+scanf("%d",n);
+if(n=='a'!!n=='e'!!n=='i'!!n=='o'!!n=='u')
+{
+printf("vowel");
+}
+else {
+printf("consonant");
+}
+}

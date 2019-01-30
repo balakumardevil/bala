95859689d3b5398a6d396c04dfdc1ba7198d9377
balakumardevil <42169755+balakumardevil@users.noreply.github.com>
2019-01-30 23:00:13 +0530

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,7 @@
+
+def Nth_of_AP(a, d, N) : 
+    return (a + (N - 1) * d) 
+a=int(input("a") 
+a=int(input("b")
+a=int(input("c")
+print( "The ", N ,"th term of the series is : ", Nth_of_AP(a, d, N)) 
